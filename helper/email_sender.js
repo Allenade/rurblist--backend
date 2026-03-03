6355de2445777e4f82011d58058861e8c9938532
Omozuas <iyanuomozua@gmail.com>
2026-03-03 19:05:29 +0100

    new

--- helper/email_sender.js
+++ helper/email_sender.js
@@ -3,6 +3,9 @@ const nodemailer = require("nodemailer");
 class SendEmails {
   static transporter = nodemailer.createTransport({
     service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
